f7fcae9ee90fffede2cfb61cfdc1165ff1b43214
PolivkaArsen <arsen.polivka@tetalab.com>
2025-01-18 23:48:01 +0200

    swagger setup

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ minLength: 8, description: 'Must contain letters and numbers.' })
   @IsString()
   @MinLength(8)
   @Matches(/^(?=.*[A-Za-z])(?=.*\d).+$/, {
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsOptional, MinLength, IsEnum, Matches, IsNotEmpty } from 'class-validator';
 import { EUserRoles } from 'src/utils/enums';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ minLength: 8, description: 'Must contain letters and numbers.' })
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long.' })
   @Matches(/^(?=.*[A-Za-z])(?=.*\d).+$/, {
@@ -16,16 +20,20 @@ export class CreateUserDto {
   })
   password: string;
 
+  @ApiProperty({ minLength: 8, description: 'Matches password' })
   @IsNotEmpty()
   passwordConfirmation: string;
 
+  @ApiProperty({ enum: EUserRoles })
   @IsEnum(EUserRoles)
   role?: EUserRoles;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   bio?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   avatarUrl?: string;
